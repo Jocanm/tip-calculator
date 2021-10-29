b6018aeaa73eb9cc73e65010b5bc8d4cece0ba3c
Jose Angarita <amjose09@gmail.com>
2021-10-29 11:10:59 -0500

    Proyecto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import './App.css';
 import logo from './images/logo.svg'
 import ResultOperation from './component/ResultOperation';
+import { nanoid } from 'nanoid';
 import { useState } from 'react';
 
 function App() {
+
+  const [bill, setBill] = useState("")
+  const [people, setPeople] = useState("")
+  const [porc, setPorc] = useState("")
+  const [porcInput, setPorcInpute] = useState("")
+
+  const reset = () => {
+    setBill("")
+    setPeople("")
+    setPorc("")
+    setPorcInpute("")
+  }
+
   return (
     <div className="flex flex-col items-center justify-center h-screen w-screen">
       <img
@@ -13,58 +27,99 @@ function App() {
       <div className="flex flex-col sm:flex-row justify-between items-center bg-white rounded-lg p-4 h-auto w-fll sm:w-auto">
         <main className="mb-4 sm:mb-0 mr-0 sm:mr-4 w-full h-full">
           <CustomInput
+            value={bill}
+            setValue={setBill}
             icono="fas fa-dollar-sign"
             title="Bill"
-            name=""
           />
           <TipButtons
             text="Select tip %"
+            porc={porc}
+            setPorc={setPorc}
+            porcInput={porcInput}
+            setPorcInpute={setPorcInpute}
           />
           <CustomInput
+            value={people}
+            setValue={setPeople}
             title="Number of People"
             icono="fas fa-user"
           />
         </main>
-        <ResultOperation />
+        <ResultOperation
+          bill={bill}
+          total={people}
+          porc={porc}
+          porcInput={porcInput}
+          reset={reset}
+        />
       </div>
     </div>
   );
 }
 
-const CustomInput = ({ value = 0, title = "hola", icono, name }) => {
+const CustomInput = ({ value, setValue, title, icono }) => {
+
+  const handleValue = ({ target }) => {
+    setValue(target.value)
+  }
+
 
   return (
     <div className="cont">
-      <span className="text-cyan-dark font-bold text-xs tracking-wider">{title}</span>
+      <div className="flex justify-between">
+        <span className="text-cyan-dark font-bold text-xs tracking-wider">{title}</span>
+        <span className={`${value==="0"?"text-xs text-red-400 font-semibold":"hidden"}`}>Can't be zero</span>
+      </div>
       <input
         type="number"
-        className="custom-input mt-1 special-input"
-        name={name}
+        className={`custom-input mt-1 special-input ${value==="0"&&"boder border-red-400 hover:border-red-400"}`}
+        value={value}
+        onChange={handleValue}
+        placeholder="0"
       />
       <i className={icono}></i>
     </div>
   )
 }
 
-const TipButtons = ({ text = "" }) => {
+const TipButtons = ({ text, porc, setPorc, porcInput, setPorcInpute }) => {
 
-  const [porcentajes] = useState(["5%", "10%", "15%", "25%", "50%"])
+  const [porcentajes] = useState([5, 10, 15, 25, 50])
+
+  const handleCustomPorc = ({ target }) => {
+    setPorcInpute(target.value)
+    if (porcInput && porc !== "") setPorc("")
+  }
+
+  const handleButtonPorc = (value) => {
+    setPorc(value)
+    if (porc && porcInput !== "") setPorcInpute("")
+  }
 
   return (
     <div className="flex flex-col mb-5 mt-3">
       <h2 className="text-cyan-dark mb-2 tracking-wider text-xs font-bold">{text}</h2>
       <div className="btn-cont grid grid-cols-2 grid-rows-3 sm:grid-cols-3 sm:grid-rows-2">
         {
-          porcentajes.map(e => (
-            <button className="boton-1 hover:bg-cyan-strong hover:text-cyan-strongest">
-              {e}
+          porcentajes.map((e) => (
+            <button
+              key={nanoid()}
+              className={`boton-1 px-7 hover:bg-cyan-strong hover:text-cyan-strongest ${(e === porc ? "bg-cyan-strong text-cyan-strongest font-black" : "bg-cyan-strongest text-white font-bold")}`}
+              onClick={() => {
+                handleButtonPorc(e)
+              }}
+            >
+              {`${e}%`}
             </button>
           ))
         }
         <input
           type="number"
-          placeholder="Custom"
+          placeholder="Custom %"
           className="tip-input text-right"
+          value={porcInput}
+          onChange={handleCustomPorc}
         />
 
       </div>
--- src/component/ResultOperation.jsx
+++ src/component/ResultOperation.jsx
@@ -1,14 +1,29 @@
 import { useEffect, useState } from "react"
 
-const ResultOperation = ({ bill, porc, total, reset }) => {
+const ResultOperation = ({ bill, porc, porcInput, total, reset }) => {
+
+    const [lastPorc, setLastPorc] = useState("")
+
+    useEffect(()=>{
+        (porcInput!=="")?(setLastPorc(porcInput)):(setLastPorc(porc))
+    },[porc,porcInput])
 
     return (
         <div className="bg-cyan-strongest rounded-lg pt-10 pb-5 sm:py-10 w-full h-full items-center flex flex-col justify-between">
             <div className="flex flex-col h-1/2">
-                <TipAumount />
-                <Total />
+                <TipAumount 
+                    bill={bill}
+                    total={total}
+                    porc={lastPorc}
+                />
+                <Total 
+                    bill={bill}
+                    total={total}
+                />
             </div>
-            <button className="bg-cyan-strong rounded-sm px-28 py-1 text-cyan-strongest font-semibold mt-6 sm:mt-0">RESET</button>
+            <button className="bg-cyan-strong rounded-sm px-28 py-1 text-cyan-strongest font-semibold mt-6 sm:mt-0 hover:shadow-xl hover:text-warmGray-50"
+            onClick={reset}
+            >RESET</button>
         </div>
     )
 }
@@ -40,7 +55,7 @@ const TipAumount = ({ bill, total, porc }) => {
 }
 
 
-const Total = ({ bill=0, total=1 }) => {
+const Total = ({ bill, total }) => {
 
     const [totalPerson, setTotalPerson] = useState(0)
 
@@ -55,7 +70,7 @@ const Total = ({ bill=0, total=1 }) => {
     return (
         <div className="grid grid-cols-2 gap-20 gap-x-14 sm:gap-20 justify-around w-full items-center h-full">
             <div className="flex flex-col justify-between">
-                <h2 className="text-white text-sm font-semibold">Tip Amount</h2>
+                <h2 className="text-white text-sm font-semibold">Total</h2>
                 <p className="text-gray-300 text-xs">/ person</p>
             </div>
             <div className="text-2xl text-cyan-strong font-bold tex">
